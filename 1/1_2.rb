aed73e6c61d63aea38f06961efa68060ffb321b8
Andrei-Mihai Nicolae <me@nclandrei.com>
2020-12-07 16:54:53 +0200

    Complete day 1 part 2

--- 1/1_2.rb
+++ 1/1_2.rb
@@ -1,1 +1,23 @@
+# frozen_string_literal: true
+
+SUM = 2020
+
+values = {}
+File.readlines('input.txt').each do |val|
+  values[val.to_i] = nil
+end
+
+def product_of_numbers_that_sum(values, sum)
+  values.each do |val, _|
+    if values.key?(sum - val)
+      return val * (sum - val)
+    end
+  end
+  nil
+end
+
+values.each do |val, _|
+  product = product_of_numbers_that_sum(values, SUM - val)
+  puts val * product unless product.nil?
+end
 
