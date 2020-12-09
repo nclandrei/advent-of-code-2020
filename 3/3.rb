f90ff1e789bf36a3aede430acdbca9cfd8245cde
Andrei-Mihai Nicolae <me@nclandrei.com>
2020-12-09 18:16:16 +0200

    Complete day 3 part 1

--- 3/3.rb
+++ 3/3.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
 
-matrix = []
+values = []
+File.readlines('input.txt').each do |line|
+  values.push(line.chars.map { |char| char == '.' ? 0 : 1 })
+end
 
+x = 0
+y = 0
+line_length = values[0].length
+tree_count = 0
+
+while y < values.length
+  if values[y][x % (line_length - 1)] == 1
+    tree_count += 1
+  end
+
+  x += 3
+  y += 1
+end
+
+puts tree_count
